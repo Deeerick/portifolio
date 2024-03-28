d6e7ea6adc5a2d018ca3b87a5bf017e0b3a8a37e
Deeerick <dspsoares@outlook.com>
2024-03-27 22:09:57 -0300

    Update 007

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+/// ABERTURA E FECHAMENTO DO MENU LATERAL NA VERSAO MOBILE ///
 const menuMobile = document.querySelector(".menu-mobile");
 const body = document.querySelector('body');
 
@@ -7,3 +8,17 @@ menuMobile.addEventListener('click', () => {
         : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
 });
+
+/// FECHA O MENU LATERAL QUANDO SELECIONADO UM DOS ITENS ///
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
+})
+
+//////
